f89066e526f312521d1cc34b11bb167b41d5f504
Kyriakos Triantafyllidis <kiriakostriadafillidis@gmail.com>
2024-10-11 20:38:39 +0300

    Cube Movement

---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    public Vector3 vector3 = Vector3.zero;
+
+    public Quaternion quaternion = Quaternion.identity;
+
+    public Vector3 vector3Scale = Vector3.zero;
+
+    void Start()
+    {
+        Debug.Log("Ela kai pou eisai");
+    }
+
+    void Update()
+    {
+        transform.position = vector3;
+        transform.rotation = quaternion;
+        transform.localScale = vector3Scale;
+    }
+}
