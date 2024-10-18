1676618261eba0b86a2cbb692aa8a17ff7694d7c
Kyriakos Triantafyllidis <kiriakostriadafillidis@gmail.com>
2024-10-18 20:49:48 +0300

    Architecture

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Question
+{
+    public string questionTest = "";
+
+    public List<Answer> answers = new List<Answer>();
+}
---
+++ Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionManager : MonoBehaviour
+{
+    public List<Question> questions = new List<Question>();
+
+}
