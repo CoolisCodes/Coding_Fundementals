7a54f51bfcd3cf45f2379418fcb23552a33f018d
izabelapajdo <izaki96@hotmail.com>
2024-11-27 18:58:59 +0200

    gamemanager experiment

--- Assets/Scripts/MVP/GameManager.cs
+++ Assets/Scripts/MVP/GameManager.cs
@@ -1,30 +1,73 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemyPrefab;  // Enemy prefab for spawning
+    public Transform[] spawnPoints; // Locations where enemies will spawn
+    public int initialEnemiesPerWave = 5;  // Number of enemies to spawn initially
+    public float initialEnemySpeed = 2f;   // Starting speed for enemies
+    public float spawnInterval = 2f;       // Time between enemy spawns
+    public float difficultyIncreaseInterval = 30f;  // How often to increase difficulty (in seconds)
 
+    private int currentWave = 1;  // The current wave number
+    private int enemiesRemaining; // Number of enemies remaining in the current wave
+    private float currentEnemySpeed; // Current speed of enemies
+    private float currentSpawnInterval;  // Current time between spawns
 
-    
-    public int gameDifficulty = 0;
-    public GroundMover groundMover;
-
-    public SpaceshipMover spaceshipMover;
-
-    public GameObject cometPrefab;
-
-    public Transform cometSpawnPoint;
     void Start()
     {
-        GameObject newComet = Instantiate(cometPrefab);
-        newComet.transform.position = cometSpawnPoint.position;
+        currentEnemySpeed = initialEnemySpeed;
+        currentSpawnInterval = spawnInterval;
+        StartCoroutine(StartWave());
+        StartCoroutine(IncreaseDifficultyOverTime());
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start a new wave
+    private IEnumerator StartWave()
     {
-        
+        enemiesRemaining = initialEnemiesPerWave + (currentWave - 1) * 2;  // Increase number of enemies with each wave
+
+        for (int i = 0; i < enemiesRemaining; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(currentSpawnInterval);
+        }
+
+        // Wait until all enemies are destroyed before starting the next wave
+        yield return new WaitUntil(() => FindObjectsOfType<Comet>().Length == 0);  // Wait until all enemies are gone
+        currentWave++;  // Increment wave number
+        StartCoroutine(StartWave());  // Start the next wave
+    }
+
+    // Spawn an enemy at a random spawn point
+    private void SpawnEnemy()
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemyPrefab, spawnPoints[spawnIndex].position, Quaternion.identity);
+    }
+
+    // Coroutine to increase difficulty over time
+    private IEnumerator IncreaseDifficultyOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(difficultyIncreaseInterval);
+
+            // Increase the enemy speed and decrease spawn interval (difficulty mechanics)
+            currentEnemySpeed += 0.5f;  // Increase enemy speed
+            currentSpawnInterval = Mathf.Max(0.5f, currentSpawnInterval - 0.1f);  // Decrease spawn interval (up to a limit)
+        }
+    }
+
+    // Method to get current game difficulty settings
+    public float GetEnemySpeed()
+    {
+        return currentEnemySpeed;
+    }
+
+    public float GetSpawnInterval()
+    {
+        return currentSpawnInterval;
     }
 }
